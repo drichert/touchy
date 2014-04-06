e672e826afb18a31b120b1da3a1dda22b273c3d1
Dan Richert <dan.richert@gmail.com>
2014-04-06 14:50:19 -0400

    mv functions

--- src/arduino/touchy.ino
+++ src/arduino/touchy.ino
@@ -9,6 +9,81 @@ int   num_freqs = 160;
 float results[num_freqs];
 float freq[num_freqs];
 
+void setup() {
+  TCCR1A = 0b10000010; // Set up frequency generator
+  TCCR1B = 0b00011001; // +
+  ICR1   = 110;
+  OCR1A  = 55;
+
+  pinMode(9, OUTPUT);  // Signal generator pin
+  pinMode(8, OUTPUT);  // Sync (test) pin
+
+  Serial.begin(115200);
+
+  // Init results
+  for(int i = 0; i < num_freqs; i++) {
+    results[i] = 0;
+  }
+}
+
+void loop() {
+  unsigned int d;
+
+  int counter = 0;
+  for(unsigned int d=0;d<N;d++)
+  {
+    int v = analogRead(0); // Read response signal
+
+    CLR(TCCR1B,0);  // Stop generator
+
+    TCNT1 = 0;     // Reload new frequency
+    ICR1  = d;     // |
+    OCR1A = d / 2; // +
+
+    SET(TCCR1B,0); // Restart generator
+
+    // Filter results
+    results[d] = results[d] * 0.5 + (float)(v) * 0.5;
+
+    freq[d] = d;
+  }
+
+  send_all(1, freq, results);
+
+  TOG(PORTB,0); // Toggle pin 8 after each sweep (good for scope)
+}
+
+/*
+                                                 .
+                                      .         ;
+         .              .              ;%     ;;
+           ,           ,                :;%  %;
+            :         ;                   :;%;'     .,
+   ,.        %;     %;            ;        %;'    ,;
+     ;       ;%;  %%;        ,     %;    ;%;    ,%'
+      %;       %;%;      ,  ;       %;  ;%;   ,%;'
+       ;%;      %;        ;%;        % ;%;  ,%;'
+        `%;.     ;%;     %;'         `;%%;.%;'
+         `:;%.    ;%%. %@;        %; ;@%;%'
+            `:%;.  :;bd%;          %;@%;'
+              `@%:.  :;%.         ;@@%;'
+                `@%.  `;@%.      ;@@%;
+                  `@%%. `@%%    ;@@%;
+                    ;@%. :@%%  %@@%;
+                      %@bd%%%bd%%:;
+                        #@%%%%%:;;
+                        %@@%%%::;
+                        %@@@%(o);  . '
+                        %@@@o%;:(.,'
+                    `.. %@@@o%::;
+                       `)@@@o%::;
+                        %@@(o)::;
+                       .%@@@@%::;
+                       ;%@@@@%::;.
+                      ;%@@@@%%:;;;.
+                  ...;%@@@@@%%:;;;;,..    Gilo97
+*/
+
 // Note: x/y arguments reversed from original code (was y then x)
 void send(int cmd, unsigned int x, unsigned int y) {
   byte x_msb    = 0,
@@ -76,46 +151,3 @@ void send_all(unsigned int cmd, float arr1[], arr2[]) {
   send(cmd + 2, 1, 1);
 }
 
-void setup() {
-  TCCR1A = 0b10000010; // Set up frequency generator
-  TCCR1B = 0b00011001; // +
-  ICR1   = 110;
-  OCR1A  = 55;
-
-  pinMode(9, OUTPUT);  // Signal generator pin
-  pinMode(8, OUTPUT);  // Sync (test) pin
-
-  Serial.begin(115200);
-
-  // Init results
-  for(int i = 0; i < num_freqs; i++) {
-    results[i] = 0;
-  }
-}
-
-void loop() {
-  unsigned int d;
-
-  int counter = 0;
-  for(unsigned int d=0;d<N;d++)
-  {
-    int v = analogRead(0); // Read response signal
-
-    CLR(TCCR1B,0);  // Stop generator
-
-    TCNT1 = 0;     // Reload new frequency
-    ICR1  = d;     // |
-    OCR1A = d / 2; // +
-
-    SET(TCCR1B,0); // Restart generator
-
-    // Filter results
-    results[d] = results[d] * 0.5 + (float)(v) * 0.5;
-
-    freq[d] = d;
-  }
-
-  send_all(1, freq, results);
-
-  TOG(PORTB,0); // Toggle pin 8 after each sweep (good for scope)
-}
